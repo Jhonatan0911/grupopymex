3e0e63fceec04e1fbd88651fffc9fd3e813dda22
Jhonatan0911 <jpinalozano911@gmail.com>
2021-04-14 19:18:33 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'grupopymex';
+  parrafo1:boolean = false;
+  parrafo2:boolean = false;
+  parrafo3:boolean = false;
+  parrafo4:boolean = false;
+  parrafo5:boolean = false;
+  parrafo6:boolean = false;
+ 
 }
+
+
